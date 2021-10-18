0cec17f6b57565a4783de2037cc81005a554c093
Umeayo Onyekachukwu <umeayo.o.h@gmail.com>
2021-10-18 21:49:49 +0100

    refactored sidebar

---
+++ src/components/tailwind/SidebarElements.tsx
@@ -0,0 +1,78 @@
+import { Link as LinkS } from "react-scroll";
+import { GetProps } from "../../types";
+import { ComponentProps, ReactNode } from "react";
+
+export const SidebarContainer = (props: {
+  isOpen: boolean;
+  onClick: () => void;
+  children: ReactNode;
+}) => (
+  <aside
+    className={`${
+      props.isOpen ? "visible top-0" : "invisible top-full"
+    } md:hidden w-screen h-screen fixed inset-0 bg-black z-50 transition-all `}
+    onClick={props.onClick}
+  >
+    {props.children}
+  </aside>
+);
+export function SidebarItems({ className, ...props }: ComponentProps<"li">) {
+  return (
+    <li
+      className={
+        "py-4 cursor-pointer hover:scale-x-110 text-center" + " " + className
+      }
+      {...props}
+    />
+  );
+}
+export function SidebarLink({ className, ...props }: GetProps<typeof LinkS>) {
+  return <LinkS className={className} {...props} />;
+}
+export const SideBarCloseIconContainer = (props: {
+  toggleIsOpen: () => void;
+  children: React.ReactNode;
+}) => (
+  <div
+    className="h-8 absolute top-10 right-10"
+    onClick={(e) => {
+      props.toggleIsOpen();
+      e.stopPropagation();
+    }}
+    children={props.children}
+  />
+);
+export const SidebarList = ({
+  className = "",
+  children,
+}: {
+  className?: string;
+  children: ReactNode;
+}) => (
+  <ul
+    className={
+      "flex flex-col justify-evenly items-stretch text-2xl text-white" +
+      " " +
+      className
+    }
+  >
+    {children}
+  </ul>
+);
+export const SidebarInnerContainer = ({
+  className = "",
+  children,
+}: {
+  className?: string;
+  children: ReactNode;
+}) => (
+  <div
+    className={
+      "flex flex-col items-stretch justify-evenly h-full w-full" +
+      " " +
+      className
+    }
+  >
+    {children}
+  </div>
+);
--- src/components/tailwind/SidebarTailwind.tsx
+++ src/components/tailwind/SidebarTailwind.tsx
@@ -1,47 +1,26 @@
 import { FaTimes } from "react-icons/fa";
 import { NavBtnContainer, NavBtn, NavBtnLink } from "./NavElement";
-import { Link as LinkS } from "react-scroll";
-import { GetProps } from "../../types";
-import { ComponentProps } from "react";
-
-function SidebarItems({ className, ...props }: ComponentProps<"li">) {
-  return (
-    <li
-      className={
-        "py-4 cursor-pointer hover:scale-x-110 text-center" + " " + className
-      }
-      {...props}
-    />
-  );
-}
-
-function SidebarLink({ className, ...props }: GetProps<typeof LinkS>) {
-  return <LinkS className={className} {...props} />;
-}
+import {
+  SidebarContainer,
+  SidebarInnerContainer,
+  SideBarCloseIconContainer,
+  SidebarList,
+  SidebarItems,
+  SidebarLink,
+} from "./SidebarElements";
 
 export function SideBarTailwind(props: {
   isOpen: boolean;
   toggleIsOpen: () => void;
 }): JSX.Element {
   return (
-    <aside
-      className={`${
-        props.isOpen ? "visible top-0" : "invisible top-full"
-      } md:hidden w-screen h-screen fixed inset-0 bg-black z-50 transition-all `}
-      onClick={props.toggleIsOpen}
-    >
-      <div className="flex flex-col items-stretch justify-evenly h-full w-full">
-        <div
-          className="h-8 absolute top-10 right-10"
-          onClick={(e) => {
-            props.toggleIsOpen();
-            e.stopPropagation();
-          }}
-        >
+    <SidebarContainer isOpen={props.isOpen} onClick={props.toggleIsOpen}>
+      <SidebarInnerContainer>
+        <SideBarCloseIconContainer toggleIsOpen={props.toggleIsOpen}>
           <FaTimes className="text-white text-3xl" />
-        </div>
+        </SideBarCloseIconContainer>
         <div className="flex flex-col justify-evenly">
-          <ul className="flex flex-col justify-evenly items-stretch text-2xl text-white">
+          <SidebarList>
             <SidebarItems>
               <SidebarLink to="about">About</SidebarLink>
             </SidebarItems>
@@ -54,7 +33,7 @@ export function SideBarTailwind(props: {
             <SidebarItems>
               <SidebarLink to="signup">Sign Up</SidebarLink>
             </SidebarItems>
-          </ul>
+          </SidebarList>
         </div>
         <NavBtnContainer className="">
           <NavBtn
@@ -66,7 +45,7 @@ export function SideBarTailwind(props: {
             <NavBtnLink to="signin">Sign In</NavBtnLink>
           </NavBtn>
         </NavBtnContainer>
-      </div>
-    </aside>
+      </SidebarInnerContainer>
+    </SidebarContainer>
   );
 }
