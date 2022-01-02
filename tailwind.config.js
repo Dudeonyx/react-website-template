819cc0da534481e7b9b7d2bed19688725b06167a
Umeayo Onyekachukwu <umeayo.o.h@gmail.com>
2022-01-02 20:41:06 +0100

    updated to cra 5, tailwind 3 & postcss 8

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,7 @@
 const { teal, emerald, fuchsia } = require('tailwindcss/colors');
 
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
     extend: {
       spacing: {
