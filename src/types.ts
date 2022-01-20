7b4154dabffe612d0adb73ecc5449931be24414b
Umeayo Onyekachukwu <umeayo.o.h@gmail.com>
2022-01-20 22:04:05 +0100

    fixed GetProps

--- src/types.ts
+++ src/types.ts
@@ -7,9 +7,7 @@ import {
 import { ComponentType } from "react";
 import { Component, PropsWithoutRef } from "react";
 
-export type GetProps<C> = C extends
-  | keyof JSX.IntrinsicElements
-  | JSXElementConstructor<any>
+export type GetProps<C> = C extends keyof JSX.IntrinsicElements
   ? ComponentProps<C>
   : C extends ComponentClass<infer P>
   ? PropsWithoutRef<P>
